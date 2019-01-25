d537445b1fb6075cee9d2fe0bbe49b05ba0a7bc7
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-25 06:07:05 -0500

    End lesson-03, add locaion authorization and set up location finder

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -8,25 +8,55 @@
 
 import UIKit
 import MapKit
-
+import CoreLocation
 
 class MapVC: UIViewController {
 
     //Outlets
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        //Center the pin location
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate {
-    
+    //Setting the zoom radius around the location
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
+
+extension MapVC: CLLocationManagerDelegate {
+    //Checking to see if the app has authorization to use the location
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }else {
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
+
