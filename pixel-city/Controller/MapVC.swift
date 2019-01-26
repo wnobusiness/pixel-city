9aa5a40388d0d4cf97e97cf52d038c035a8f3e65
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-25 23:04:19 -0500

    End lesson-05, edit mapview pin, changed the color

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -46,6 +46,17 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
 }
 
 extension MapVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let pinAnnotation =  MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        pinAnnotation.pinTintColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
+        pinAnnotation.animatesDrop = true
+        
+        return pinAnnotation
+    }
+    
     //Setting the zoom radius around the location
     func centerMapOnUserLocation() {
         guard let coordinate = locationManager.location?.coordinate else { return }
