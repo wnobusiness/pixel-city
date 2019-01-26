948e8246f4e9b33dc2466a9a86323a1708c453d1
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-25 22:00:40 -0500

    End lesson-04, add the double tap gesture, add 'DroppablePin.swift', and add the pin

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     //Outlets
     @IBOutlet weak var mapView: MKMapView!
@@ -26,6 +26,14 @@ class MapVC: UIViewController {
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
+        addDoubleTap()
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        mapView.addGestureRecognizer(doubleTap)
     }
 
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
@@ -43,6 +51,24 @@ extension MapVC: MKMapViewDelegate {
         guard let coordinate = locationManager.location?.coordinate else { return }
         let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer) {
+        removePin()
+        let touchPoint = sender.location(in: mapView)
+        let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
+        mapView.addAnnotation(annotation)
+        
+        let coordinateRegion = MKCoordinateRegion(center: touchCoordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func removePin() {
+        for annotation in mapView.annotations {
+            mapView.removeAnnotation(annotation)
+        }
     }
 }
 
---
+++ pixel-city/View/DroppablePin.swift
@@ -0,0 +1,22 @@
+//
+//  DroppablePin.swift
+//  pixel-city
+//
+//  Created by Krishna Torrence on 1/25/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+    
+    init(coordinate: CLLocationCoordinate2D, identifier: String) {
+        self.coordinate = coordinate
+        self.identifier = identifier
+        super.init()
+    }
+    
+}
